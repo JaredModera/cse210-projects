f4c6f0dc6c1482969fc5867884864b3234bb5733
JaredModera <jareditapablo@gmail.com>
2023-09-15 13:34:15 -0600

    Finished the C# Prep 3 :)

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("Please Write the magic number: ");
+        string answer = Console.ReadLine();
+        int magicNumber = int.Parse(answer);
+    
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("Make a guess on the magic number: ");
+            guess = int.Parse(Console.ReadLine()); //new way of converting learned.
+
+            if (guess > magicNumber) {
+            Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber) {
+                Console.WriteLine("Higher");
+            }
+            else {
+                Console.WriteLine("You guessed it!");
+            }
+    
+        }
+
+        
     }
 }
