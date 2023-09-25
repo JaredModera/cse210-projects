c01beb658f4d9fce8bcddd117d001f2001f36304
JaredModera <jareditapablo@gmail.com>
2023-09-25 17:34:53 -0500

    Finished the Week03 Learning Activity and left a few extra comments

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction frt1 = new Fraction();
+        Console.WriteLine(frt1.GetFractionString());
+        Console.WriteLine(frt1.GetDecimalValue());
+
+        Fraction frt2 = new Fraction(5);
+        Console.WriteLine(frt2.GetFractionString());
+        Console.WriteLine(frt2.GetDecimalValue());
+
+        Fraction frt3 = new Fraction(3, 4);
+        Console.WriteLine(frt3.GetFractionString());
+        Console.WriteLine(frt3.GetDecimalValue());
+
+        Fraction frt4 = new Fraction(1, 3);
+        Console.WriteLine(frt4.GetFractionString());
+        Console.WriteLine(frt4.GetDecimalValue());
+        
     }
 }
